f62164b874c6e5adfe21dc8d59ae4c756f28d976
Monicah Cloud <monicahcloud@gmail.com>
2024-11-12 21:00:57 -0600

    theme added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-0">
+      <div className="align-element grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-0">
         <div className="col-span-2  p-2">
           <NavBar />
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,40 @@
+import { useState, useEffect } from 'react'
 import logo from '../assets/logo.webp'
+import { BsMoonFill, BsSunFill } from 'react-icons/bs'
+
+const themes = {
+  dark: 'dark',
+  light: 'light',
+}
+
+const getThemeFromLocalStorage = () => {
+    return localStorage.getItem('theme') || themes.light
+}
 
 const NavBar = () => {
+  const [theme, setTheme] = useState(themes.light)
+  const handleTheme = () => {
+    const { light, dark } = themes
+    const newTheme = theme === light ? dark : light
+    setTheme(newTheme)
+  }
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <>
-      <div className="flex space-x-4 w-[60px] h-[60px] flex items-center justify-center">
-        <img src={logo} alt="logo" />
-        <h2>Navbar</h2>
+      <div className=" navbar align-element ">
+        <img className="navbar-start w-[80px] h-[80px]" src={logo} alt="logo" />
+        <h2 className=" navbar-center text-3xl items-center">Navbar</h2>
+        <div className="navbar-end ">
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            <BsSunFill className="swap-on h-4 w-4 " />
+            <BsMoonFill className="swap-off h-4 w-4 " />
+          </label>
+        </div>
       </div>
     </>
   )
