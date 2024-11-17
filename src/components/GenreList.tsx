08efb63b431a8b831cbdf50a59927ddbc195fd50
Monicah Cloud <monicahcloud@gmail.com>
2024-11-17 10:52:07 -0600

    clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,37 +1,44 @@
-import useGenres from '../hooks/useGenres'
 import Spinner from './Spinner'
+import useGenres, { Genre } from '../hooks/useGenres'
+import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
   selectedGenre: Genre | null
 }
+
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres()
+
   if (error) return null
+
   if (isLoading) return <Spinner />
+
   return (
-    <>
-      <ul className="space-y-2">
+    <div>
+      <h2 className="text-2xl font-semibold mt-9 mb-3">Genres</h2>
+      <ul>
         {data.map((genre) => (
           <li key={genre.id} className="py-2">
-            <div className="flex items-center">
+            <div className="flex items-center space-x-3">
               <img
-                className="w-8 h-8 rounded-md object-cover"
-                src={genre.image_background}
+                className="w-8 h-8 rounded-lg object-cover"
+                src={getCroppedImageUrl(genre.image_background)}
                 alt={genre.name}
               />
-              <a
-                href="#"
+              <button
+                className={`text-left text-md font-normal ${
+                  genre.id === selectedGenre?.id ? 'font-bold' : ''
+                }`}
                 onClick={() => onSelectGenre(genre)}
-                className={ `ml-3 ${genre.id === selectedGenre?.id ? 'font-extrabold' : 'font-normal'}  text-slate-400 hover:underline `}
               >
                 {genre.name}
-              </a>
+              </button>
             </div>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -5,6 +5,13 @@ const getCroppedImageUrl = (url: string) => {
 
   const target = 'media/'
   const index = url.indexOf(target) + target.length
-  return url.slice(0, index) + 'crop/600/400' + url.slice(index)
+
+  // Ensure that the 'crop/600/400/' string is added only if the target is found
+  if (index !== -1) {
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
+  }
+
+  return noImage // In case the 'media/' part is not found, return the placeholder
 }
+
 export default getCroppedImageUrl
