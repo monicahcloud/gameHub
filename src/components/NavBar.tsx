41ccc4fb40a05634cc07221d9ec6516c63783f58
Monicah Cloud <monicahcloud@gmail.com>
2024-12-10 15:23:17 -0600

    fixed nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
@@ -7,7 +8,7 @@ import SearchInput from './SearchInput'
 const NavBar = () => {
   return (
     <div className="flex items-center gap-4 p-2">
-      <img src={logo} alt="Logo" className="w-16 h-16" />
+      <Link to='/'><img src={logo} alt="Logo" className="w-16 h-16" object-cover /></Link>
       <SearchInput />
       <ColorModeSwitch />
     </div>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,8 +2,6 @@ import { useParams } from 'react-router-dom'
 import useGame from '../hooks/useGame'
 import  Spinner  from '../components/Spinner'
 import ExpandableText from '../components/ExpandableText'
-import DefinintionItem from '../components/DefinintionItem'
-import CriticScore from '../components/CriticScore'
 import GameAttributes from '../components/GameAttributes'
 import GameTrailer from '../components/GameTrailers'
 import GameScreenshots from '../components/GameScreenshots'
@@ -16,13 +14,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error
   return (
     <>
-      <h1 className="text-4xl font-semibold text-slat-800 prose">
-        {game.name}
-      </h1>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game}/>
-      <GameTrailer gameId={game.id}/>
-      <GameScreenshots gameId={game.id}/>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="space-y-5">
+          <h2 className="text-4xl font-bold text-slat-800">{game.name}</h2>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+          <GameAttributes game={game} />
+        </div>
+        <div className="space-y-5">
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </div>
+      </div>
     </>
   )
 }
